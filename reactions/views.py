f21931508aabb47cb3154aebae26f26d4d107440
Jimmy Tron <oduorjames@hotmail.com>
2022-11-30 08:32:11 +0300

    Modify the views
    have the views display meaningful info
    
    	new file:   reactions/migrations/0002_alter_reaction_choice_reaction_choice.py
    	modified:   reactions/views.py

--- reactions/views.py
+++ reactions/views.py
@@ -6,14 +6,17 @@ from .models import Stori
 def stori_list(request):
     MAX_OBJECTS = 20
     mastori = Stori.objects.all()[:MAX_OBJECTS]
-    data = {"results": list(mastori.values("stori","created_by__account.user","created"))}
+    data = {"results": list(mastori.values("title","description","created_by__alias","created"))}
     return JsonResponse(data)
 
 def stori_detail(request, pk):
     stori = get_object_or_404(Stori, pk=pk)
     data = {"results": {
-        "stori_title": stori.title,
-        "created_by": stori.created_by.Account,
+        "Stori Title": stori.title,
+        "Stori": stori.stori,
+        "Stori Description": stori.description,
+        "Stori Category": stori.category.category,
+        "created by": stori.created_by.alias,# user
         "created": stori.created
     }}
     return JsonResponse(data)
